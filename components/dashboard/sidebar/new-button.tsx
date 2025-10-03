28309177c4b93c61ed2c86b435d64bc6b09a98f2
Rumon360 <hmk.rumon@gmail.com>
2025-10-03 19:36:18 +0600

    feat: new button

---
+++ components/dashboard/sidebar/new-button.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Hint from "@/components/hint";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { CreateOrganization } from "@clerk/nextjs";
+import { PlusIcon } from "lucide-react";
+
+function NewButton() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Hint
+          label="Create Organization"
+          side="right"
+          align="start"
+          sideOffset={10}
+        >
+          <button className="aspect-square bg-white/25 flex justify-center items-center rounded-md opacity-60 hover:opacity-100 transition">
+            <PlusIcon className="text-white" />
+          </button>
+        </Hint>
+      </DialogTrigger>
+
+      <DialogContent className="max-w-[calc(100%-2rem)] lg:max-w-lg w-full p-0 border-none shadow-none">
+        <CreateOrganization
+          appearance={{
+            variables: { colorShadow: "none" },
+            elements: {
+              rootBox: { width: "100%", height: "100%" },
+              cardBox: { width: "100%", height: "100%" },
+            },
+          }}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default NewButton;
