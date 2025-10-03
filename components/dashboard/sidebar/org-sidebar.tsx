b85a93cf8cfc3cdcff9c1fa38a6cec7659c3c5b7
Rumon360 <hmk.rumon@gmail.com>
2025-10-03 19:56:00 +0600

    feat: org switcher

--- components/dashboard/sidebar/org-sidebar.tsx
+++ components/dashboard/sidebar/org-sidebar.tsx
@@ -1,9 +1,45 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const poppins = Poppins({ subsets: ["latin"], weight: "600" });
+
 function OrgSidebar() {
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
-      OrgSidebar
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 ">
+      <Link href={"/"}>
+        <div className="flex items-center gap-x-2">
+          <Image src={"images/logo.svg"} alt="logo" height={40} width={40} />
+          <span className={cn("font-semibold text-xl", poppins.className)}>
+            Moodboard
+          </span>
+        </div>
+      </Link>
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white",
+            },
+          },
+        }}
+      />
     </div>
   );
 }
