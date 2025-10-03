7d19713bb000cf53e3fd1d972c3b2352c7f62e64
Rumon360 <hmk.rumon@gmail.com>
2025-10-03 19:35:46 +0600

    feat: user button

--- components/dashboard/navbar.tsx
+++ components/dashboard/navbar.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { UserButton } from "@clerk/nextjs";
+
 function Navbar() {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">Navbar</div>
+    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+      <div className="hidden lg:flex lg:flex-1"></div>
+      <UserButton />
+    </div>
   );
 }
 
