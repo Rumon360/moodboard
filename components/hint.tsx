5284ec22f3a1f8bf32d530870f0249d6f7bbbd4a
Rumon360 <hmk.rumon@gmail.com>
2025-10-03 19:35:32 +0600

    feat: hint tooltip component

---
+++ components/hint.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+type Props = {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+};
+
+function Hint({
+  label,
+  children,
+  align,
+  alignOffset,
+  side,
+  sideOffset,
+}: Props) {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          alignOffset={alignOffset}
+          className=" text-white bg-black border-black"
+        >
+          <p className="font-semibold capitalize">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
+
+export default Hint;
