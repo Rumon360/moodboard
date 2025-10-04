dd38de197b4d8c16884ec5a4494333572d0789c2
Rumon360 <hmk.rumon@gmail.com>
2025-10-04 15:45:06 +0600

    feat: create board function

--- components/dashboard/empty-boards.tsx
+++ components/dashboard/empty-boards.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
 
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
+import useApiMutation from "@/hooks/use-api-mutation";
+
 function EmptyBoards() {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      title: "Untitled",
+      orgId: organization.id,
+    });
+  };
+
   return (
     <div className="h-full flex flex-col justify-center items-center">
       <Image
@@ -17,7 +35,9 @@ function EmptyBoards() {
         Start by creating a board for your oganization.
       </p>
       <div className="mt-6">
-        <Button size={"lg"}>Create Board</Button>
+        <Button disabled={pending} onClick={onClick} size={"lg"}>
+          Create Board
+        </Button>
       </div>
     </div>
   );
